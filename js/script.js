f837ecc762f7b6e6c3fc85ecb1c31a84915835c3
Austen Brower <austenbrower@Austen-Browers-MacBook-Pro.local>
2018-05-07 23:34:18 -0400

    Shop Tree Updates
    -Hot + Milk Drinks
    -Cafe Roulette

--- js/script.js
+++ js/script.js
@@ -9,12 +9,23 @@ $(document).ready(function(){
     $('#temperatureSection').hide();
   }
 
+  // Milk Section
+
   function hotNoMilkFunction () {
     console.log('Find the no milk options');
     event.preventDefault();
     $('#hotNoMilkSection').slideDown();
     $('#hotMilkSection').hide();
   }
+
+  function hotYesMilkFunction () {
+    console.log('Find the yes milk options');
+    event.preventDefault();
+    $('#hotYesMilkSection').slideDown();
+    $('#hotMilkSection').hide();
+  }
+
+  // Drink Section
 
   function dripShowFunction () {
     console.log('Show the drip cafe options');
@@ -29,14 +40,69 @@ $(document).ready(function(){
   }
 
   function pourOverShowFunction () {
-    console.log('Show the espresso cafe options');
+    console.log('Show the pour over cafe options');
     $('#hotNoMilkSection').hide();
     $('#hotNoMilkPourOver').slideDown ();
   }
 
+  function cortadoShowFunction () {
+    console.log('Show the cortado cafe options');
+    $('#hotYesMilkSection').hide();
+    $('#hotMilkCortado').slideDown ();
+  }
+
+  function macchiatoShowFunction () {
+    console.log('Show the macchiato cafe options');
+    $('#hotYesMilkSection').hide();
+    $('#hotMilkMacchiato').slideDown ();
+  }
+
+  function flatWhiteShowFunction () {
+    console.log('Show the flat white cafe options');
+    $('#hotYesMilkSection').hide();
+    $('#hotMilkFlatWhite').slideDown ();
+  }
+
+  function cappuccinoShowFunction () {
+    console.log('Show the cappuccino cafe options');
+    $('#hotYesMilkSection').hide();
+    $('#hotMilkCappuccino').slideDown ();
+  }
+
+  function latteShowFunction () {
+    console.log('Show the latte cafe options');
+    $('#hotYesMilkSection').hide();
+    $('#hotMilkLatte').slideDown ();
+  }
+
+  
+
   $('#hot').click(hot);
+
+
   $('#hotNoMilk').click(hotNoMilkFunction);
+  $('#hotYesMilk').click(hotYesMilkFunction);
+
+
   $('#drip').click(dripShowFunction);
   $('#espresso').click(espressoShowFunction);
   $('#pourOver').click(pourOverShowFunction);
+  $('#cortado').click(cortadoShowFunction);
+  $('#macchiato').click(macchiatoShowFunction);
+  $('#flatWhite').click(flatWhiteShowFunction);
+  $('#cappuccino').click(cappuccinoShowFunction);
+  $('#latte').click(latteShowFunction);
+
+
+});
+
+$(document).ready(function() {
+
+  var array=["Swings Coffee", "Gregory's Coffee", "La Colombe", "Filter", "Qualia Coffee"];
+  $('#randomButton').bind('click', function() {
+
+      var random = array[Math.floor(Math.random() * array.length)];
+
+      $("h1").html(random);
+  });
 });
